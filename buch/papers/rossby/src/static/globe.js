39a8eada238ce799a9dfd28b0f1eadba5c1db483
Your Name <michael.schmid2@ost.ch>
2025-03-14 07:31:36 +0100

    added sphere images, needs aligment to points

--- buch/papers/rossby/src/static/globe.js
+++ buch/papers/rossby/src/static/globe.js
@@ -3,16 +3,20 @@ let r = 0;
 let thetaMaxSlider, phiMaxSlider;
 let densitySlider;
 let thetaMax, phiMax;
-let density = 50;
+let density = 100;
+
+function preload() {
+  // Load an Earth texture with oceanic contours (replace with a better one if needed)
+  earthTexture = loadImage('/static/images/world.jpg');
+}
 
 function setup(){
-  createCanvas(700, 700, WEBGL);//size(600, 400);
-  angleMode(DEGREES);
+  createCanvas(windowWidth, windowHeight, WEBGL);//size(600, 400);
   colorMode(HSB);
   stroke(321, 38, 80);
   strokeWeight(2);
   noFill();
-
+  angleMode(RADIANS)
   r = width/4;
 
 }
@@ -21,18 +25,25 @@ function draw(){
   background(230, 50, 15);
   orbitControl(4, 4);//Mouse control
 
-  rotateY(90);
-  rotateZ(65);
-  for(let phi = 0; phi < 360; phi += 360/density){
-    beginShape();
-    for(let theta = 0; theta < 360; theta += 360/density){
+  for(let phi = 0; phi < TWO_PI; phi += TWO_PI/density){
+    for(let theta = 0; theta < TWO_PI; theta += TWO_PI/density){
       let x = r * cos(phi);
       let y = r * sin(phi) * sin(theta);
       let z = r * sin(phi) * cos(theta);
+      colorMode(HSB);
+      stroke(321, 38, 80);
+      strokeWeight(5);
       point(x, y, z);
     }
-    endShape(CLOSE);
   }
+  push();
+  noStroke();
+  fill(255,255,255,100);
+  texture(earthTexture);
+  sphere(r);
+
+  pop();
+
 
   let mappedDensity = int(map(density, 13, 72, 1, 60));
 }
