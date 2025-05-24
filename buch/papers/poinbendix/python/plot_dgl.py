1ffe67efb30739246fe51dca0620531ee0a9447a
Raphael <raphael.unterer@ost.ch>
2025-05-24 12:30:35 +0200

    wip cleaned up presentation and added a second example dgl

--- buch/papers/poinbendix/python/plot_dgl.py
+++ buch/papers/poinbendix/python/plot_dgl.py
@@ -8,15 +8,28 @@ def system(t, z):
     dydt = -x + 0.1*(1 - x**2)*y  # Van der Pol example
     return [dxdt, dydt]
 
+
+alpha = 1 / 8
+epsilon = 0.1
+b = 2
+gamma = 3 / 4
+r = 1 / 4
+def recharge_oscillator(t, z):
+    x, y = z
+    dxdt = -x + gamma*(b*x + y) - epsilon*(b*x + y)**3
+    dydt = -r*y - alpha* b * x
+    return [dxdt, dydt]
+
 # Time span and initial conditions
-t_span = [0, 50]
-t_eval = np.linspace(*t_span, 1000)
-initial_conditions = [[0, 0], [0.2, 0.7], [1, -1], [-2, 0]]
+t_span = [0, 10000]
+t_eval = np.linspace(*t_span, 100000)
+# initial_conditions = [[0, 0], [0.2, 0.7], [1, -1], [-2, 0]]
+initial_conditions = [[1, -1]]
 
 # Plotting
 for z0 in initial_conditions:
-    sol = solve_ivp(system, t_span, z0, t_eval=t_eval)
-    plt.plot(sol.y[0], sol.y[1], "*")
+    sol = solve_ivp(recharge_oscillator, t_span, z0, t_eval=t_eval)
+    plt.plot(sol.y[0], sol.y[1])
 
 plt.xlabel('x')
 plt.ylabel('y')
