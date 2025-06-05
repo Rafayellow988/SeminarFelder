96cb061c430cceece387993f30e5d62e19e4d0df
Raphael <raphael.unterer@ost.ch>
2025-06-05 22:44:14 +0200

    updated presentation

--- buch/papers/poinbendix/python/plot_dgl.py
+++ buch/papers/poinbendix/python/plot_dgl.py
@@ -5,11 +5,6 @@ from enable_export import enable_export
 
 enable_export()
 
-# def system(t, z):
-#     x, y = z
-#     dxdt = y
-#     dydt = -x + 0.1*(1 - x**2)*y  # Van der Pol example
-#     return [dxdt, dydt]
 
 
 alpha = 1 / 8
@@ -34,12 +29,23 @@ class RechargeOscillator():
 
 # Time span and initial conditions
 ro = RechargeOscillator(alpha, epsilon, b, gamma, r)
-t_span = [0, 1000]
-t_eval = np.linspace(*t_span, 10000)
+t_span = [0, 100]
+t_eval = np.linspace(*t_span, 1000)
 # initial_conditions = [[0, 0], [0.2, 0.7], [1, -1], [-2, 0]]
-initial_conditions = [[1, -1], [1, 2], [-0.25, 2], [-0.75, 1], [-1, -1]]
+initial_conditions = [[0, 0], [1, -1], [1, 2], [-0.25, 2], [-0.75, 1], [-1, -1]]
 
 # Plotting
+fig, ax = plt.subplots()
+for z0 in initial_conditions:
+    sol = solve_ivp(ro.run, t_span, z0, t_eval=t_eval)
+    if np.sum(np.diff(sol.y[0])) == 0:
+        ax.plot(sol.y[0], sol.y[1], ".")
+    ax.plot(sol.y[0], sol.y[1])
+
+plt.grid()
+fig.set_size_inches(w=4.5, h=2.5)
+fig.savefig("../images/recharge_oscillator.pgf")
+
 fig, ax = plt.subplots()
 for z0 in initial_conditions:
     sol = solve_ivp(ro.run, t_span, z0, t_eval=t_eval)
@@ -47,7 +53,7 @@ for z0 in initial_conditions:
 
 plt.grid()
 fig.set_size_inches(w=4.5, h=2.5)
-fig.savefig("../images/recharge_oscillator.pgf")
+fig.savefig("../images/recharge_oscillator_fall_2.pgf")
 
 # Experiment 2: wiggle on parameter alpha
 z0 = [1, -1]
@@ -74,3 +80,23 @@ plt.grid()
 plt.legend()
 fig.set_size_inches(w=4.5, h=2.5)
 fig.savefig("../images/recharge_oscillator_gamma.pgf")
+
+def fixed_point_omega_set(t, z):
+    x, y = z
+    dxdt = -x + y*(1-x**2-y**2)
+    dydt = -y - x*(1-x**2-y**2)
+    return [dxdt, dydt]
+
+
+initial_conditions = [[0, 0], [1, -1], [1, 2], [-0.25, 2], [-0.75, 1], [-1, -1]]
+fig, ax = plt.subplots()
+for z0 in initial_conditions:
+    sol = solve_ivp(fixed_point_omega_set, t_span, z0, t_eval=t_eval)
+    if np.sum(np.diff(sol.y[0])) == 0:
+        ax.plot(sol.y[0], sol.y[1], ".")
+    ax.plot(sol.y[0], sol.y[1])
+
+plt.grid()
+plt.show()
+fig.set_size_inches(w=4.5, h=2.5)
+fig.savefig("../images/fixed_point_omega_set.pgf")
